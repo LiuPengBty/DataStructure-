e94adc69e9f74a1a6f6546a1a84b8f167a24930a
刘朋 <liupeng@liupengdeMacBook-Pro-3.local>
2017-03-29 22:40:10 +0800

    3.6

---
+++ 3.6线性表的链式存储结构/3.6线性表的链式存储结构/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  3.6线性表的链式存储结构
+//
+//  Created by 刘朋 on 2017/3/29.
+//  Copyright © 2017年 刘朋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
